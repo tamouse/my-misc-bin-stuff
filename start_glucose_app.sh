2dffb00eebf8c3c2761eeb30aeef6791a1a6a5b4
Tamara Temple <tamouse@gmail.com>
2017-03-19 00:56:08 -0500

    2017-03-19T00:56:07

--- start_glucose_app.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh 
-cd $HOME/Library/WebServer/glucose
-export RAILS_ENV=production
-export SECRET_KEY_BASE=`bin/rake secret`
-/usr/bin/bundle exec unicorn --config-file config/unicorn.rb --daemonize
